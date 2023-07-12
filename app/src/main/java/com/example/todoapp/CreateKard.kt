155cbf71033b31d4f21bdc09862096d5211faa4c
Hemant <20211489@sbsstc.ac.in>
2023-07-13 02:03:20 +0530

    project todo app

---
+++ app/src/main/java/com/example/todoapp/CreateKard.kt
@@ -0,0 +1,44 @@
+package com.example.todoapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class CreateKard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_kard)
+
+        val createTitle = findViewById<EditText>(R.id.create_title)
+        val createPriority = findViewById<EditText>(R.id.create_priority)
+        val savebtn = findViewById<Button>(R.id.save_btn)
+
+        var database = Room.databaseBuilder(applicationContext, MyDatabase::class.java, "to_do")
+            .allowMainThreadQueries().build()
+
+        savebtn.setOnClickListener {
+            if (createTitle.text.toString().trim { it <= ' ' }.isNotEmpty()
+                && createPriority.text.toString().trim { it <= ' ' }.isNotEmpty()
+            ) {
+                val title = createTitle.text.toString()
+                val priority = createPriority.text.toString()
+                DataObject.setData(title, priority)
+
+                GlobalScope.launch {
+                    database.dao().insertTask(Entity(0,title,priority))
+                }
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/DAO.kt
@@ -0,0 +1,28 @@
+package com.example.todoapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface DAO {
+
+    @Insert
+    suspend fun insertTask(entity: com.example.todoapp.Entity)
+
+    @Update
+    suspend fun updateTask(entity: com.example.todoapp.Entity)
+
+    @Delete
+    suspend fun deleteTask(entity: com.example.todoapp.Entity)
+
+    @Query("Delete from to_do")
+    suspend fun deleteAll()
+
+    @Query("Select * from to_do")
+    suspend fun getTask(): List<CardInfo>
+
+}
